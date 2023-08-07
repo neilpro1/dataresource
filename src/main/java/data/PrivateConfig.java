531eb2661ace9b19904710e407915187b910cf72
neilpro1 <neilrichard067@gmail.com>
2023-08-07 02:53:25 +0100

    with private config

--- src/main/java/data/PrivateConfig.java
+++ src/main/java/data/PrivateConfig.java
@@ -2,6 +2,6 @@ package data;
 
 public class PrivateConfig {
 	
-	public final static String API_KEY = "0I22t9E0vDwbmdh8uHovZFGb4xbegHw5kRBMnyH3fkZiizQXBlJwek5UHlZZf3cf";
-	public final static String SECRET_KEY = "yZlVIRjQ4CUhxWfKmIJz1ISVX5zDsxJV1s1aRig38ra9Cqv02S04SCnmM3ZV3tG1";
+	public final static String API_KEY = "#";
+	public final static String SECRET_KEY = "#";
 }
