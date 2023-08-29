b3b524cedf813c4212ce25141fb3ea4a9290a61b
neilpro1 <neilrichard067@gmail.com>
2023-08-29 11:16:40 +0100

    last version

--- src/main/java/data/Test.java
+++ src/main/java/data/Test.java
@@ -8,8 +8,10 @@ import struct.DiskList;
 
 public class Test{
 	public static void main(String[] args) {
-		Data data = new Data(String.valueOf(Paths.get("").toAbsolutePath().getParent()));
+		Data data = new Data(String.valueOf(Paths.get("").toAbsolutePath().getParent()), "BTCUSDT", "1s");
 		
-		data.download("BTCUSDT", "1s", 1000, null, true);
+		String header[] = {"open time", "open price", "low price"};
+		data.donwload(1000, header, 0, false);
+		
 	}
 }
